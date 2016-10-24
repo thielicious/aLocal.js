7025fe58f3f6088335f28960f0f6b6edd60cea65
x7ee1 <x7ee1@gmx.de>
2016-10-24 15:12:13 +0200

    format fix

--- autoLocal.js
+++ autoLocal.js
@@ -7,15 +7,15 @@
 
 (autoLocal = function() {						// self-execution
 	
-	var localhost = "http://localhost/",		// this will replace the XAMPP folder
-		regex = /file:///C://xampp/htdocs//g;	// default XAMPP folder				
+	var localhost = "http://localhost/",				// this will replace the XAMPP folder
+		regex = /file:///C://xampp/htdocs//g;			// default XAMPP folder				
 
 	this.current =()=> { 						// turns the current path into a string
 
 		return window.location.toString();
 	}					
 
-	autoLocal.prototype.change =(()=> {			// checks if it matches and modifies the current path
+	autoLocal.prototype.change =(()=> {				// checks if it matches and modifies the current path
 
 		if (this.current().match(regex)) {
 
@@ -23,7 +23,7 @@
 				localhost + this.current().substring(
 					this.current().lastIndexOf(
 					
-						(()=> { 				// catches the page folder inside htdocs
+						(()=> { 		// catches the page folder inside htdocs
 						
 							var folder = this.current().substring(
 								this.current().lastIndexOf(
@@ -35,8 +35,8 @@
 					)
 				);
 
-			window.location.href = modified;	// spits out the modified URL and refresh page
+			window.location.href = modified;		// spits out the modified URL and refresh page
 		}
 	})();
 	
-})();
+})(); // EOL
